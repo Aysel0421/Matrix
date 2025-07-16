c2f2fda7d253be63ce30411c1f36814a64f0d2a5
Kamala03 <133358404+Kamala03@users.noreply.github.com>
2025-07-16 12:10:59 +0400

    feature/4 (#4)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hiii");
+        System.out.println("Helloooooo");
         //System.out.println("Hiii");
     }
 }
